35fcae8d1adb7ad496bdce655de3aab54e0cbb29
Lucas Thomaz <lucasthomazr@hotmail.com>
2021-06-02 22:28:14 -0300

    Implemented posts list on posts page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,13 +1,26 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-
+import { RichText } from 'prismic-dom';
 import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
 
-export default function Posts() {
+interface PostProps {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsPageProps {
+  posts: PostProps[]
+}
+
+export default function Posts({ posts }: PostsPageProps) {
   return (
     <>
       <Head>
@@ -16,21 +29,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Createing Monorepo with Lerna</strong>
-            <p>AIushaiduhsa asoidoasi oaisjdao doaioiaso oaisoasj aoisjs oiaas</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Createing Monorepo with Lerna</strong>
-            <p>AIushaiduhsa asoidoasi oaisjdao doaioiaso oaisoasj aoisjs oiaas</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Createing Monorepo with Lerna</strong>
-            <p>AIushaiduhsa asoidoasi oaisjdao doaioiaso oaisoasj aoisjs oiaas</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -47,9 +52,20 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100
   });
 
-  console.log('response', response)
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: format(new Date(post.last_publication_date), 'dd \'de\' MMMM \'de\' yyyy ', {
+        locale: ptBR
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
